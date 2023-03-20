b03640657fa0402a2e30d1a93e5cc450e8fd2637
Jascandevelop <jasmorrell.dev@gmail.com>
2023-03-20 12:20:39 -0400

    First commit

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,23 @@
+from translator import english_to_french, french_to_english
+import unittest
+
+
+class TestTranslator(unittest.TestCase):
+
+    '''tests for null input'''
+    def test_null(self):
+        self.assertEqual(english_to_french(None), " ", "null")
+        self.assertEqual(french_to_english(None), " ", "null")
+
+    '''tests English to French'''
+    def test_english(self):
+        self.assertEqual(english_to_french("Hello"), "Bonjour", "incorrect")
+
+    '''tests French to English'''
+    def test_frenchToEnglish(self):
+        self.assertEqual(french_to_english("Bonjour"), "Hello", "incorrect")
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
