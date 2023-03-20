27fd37631137d1d6ca1617634f604dc45c21f427
Jascandevelop <jasmorrell.dev@gmail.com>
2023-03-20 14:46:07 -0400

    Fixed test cases.

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -6,16 +6,18 @@ class TestTranslator(unittest.TestCase):
 
     '''tests for null input'''
     def test_null(self):
-        self.assertEqual(english_to_french(), "", "null")
-        self.assertEqual(french_to_english(), "", "null")
+        self.assertEqual(english_to_french(), '', 'null')
+        self.assertEqual(french_to_english(), '', 'null')
 
     '''tests English to French'''
     def test_english(self):
-        self.assertEqual(english_to_french("Hello"), "Bonjour", "incorrect")
+        self.assertNotEqual(english_to_french(None), 'Bonjour')
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
 
     '''tests French to English'''
     def test_frenchToEnglish(self):
-        self.assertEqual(french_to_english("Bonjour"), "Hello", "incorrect")
+        self.assertNotEqual(french_to_english(None), 'Hello')
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
 
 
 if __name__ == '__main__':
